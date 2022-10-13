467e8cea7a416e5608b5d3e0ac8463289a5a7b97
alexey <sorokinnn73@mail.ru>
2022-10-13 23:18:06 +0400

    закончили семинар

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число А");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите целое число Б");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num = num1 * num1;
+if (num == num2)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
